c0489965b38de5b27bf84c1021ed11885b421f00
Ostap <ostapstephan@gmail.com>
2019-03-25 16:03:25 -0400

    got script running however have a bug with some pages not loading all the urls

---
+++ scrape.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+# browser = webdriver.Firefox()
+
+browser.get('https://www.bloomingdales.com/shop/product/stuart-weitzman-womens-juniper-70-leather-pointed-toe-booties?ID=3115201&CategoryID=19212')
+
+
+
+print(browser.title)
